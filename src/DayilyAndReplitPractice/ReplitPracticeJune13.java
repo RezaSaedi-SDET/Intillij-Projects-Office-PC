a3ceac30d3d67f151ae00f9e8592ef572e487621
MRezaSaedi <mreza.saedi@gmail.com>
2022-06-13 21:18:20 -0700

    06-13-2022 2nd commit

--- src/DayilyAndReplitPractice/ReplitPracticeJune13.java
+++ src/DayilyAndReplitPractice/ReplitPracticeJune13.java
@@ -1,16 +1,106 @@
 package DayilyAndReplitPractice;
 
+import java.util.Locale;
+
 public class ReplitPracticeJune13 {
 
-    //For you to do:
-    //Create a String "name" and assign the value "Timmy" to it.
-    //Find out how many characters are there in the given String.
-
-    String name="Timmy";
-
-
-
     public static void main(String[] args) {
+
+        //For you to do: String “abracadabra alakazam”;
+        //
+        //Print out the position of the first occurrence of "c".
+        //Print out the position of the first occurrence of " ".
+        //Print out the position of the first occurrence of the variable target1.
+        //Print out the position of the first occurrence of the variable target2.
+
+        String str = "abracadabra alakazam";
+        String target1 = "dab";
+        String target2 = "ABRA";
+
+        System.out.println(str.indexOf("c"));
+        System.out.println(str.indexOf(" "));
+        System.out.println(target1.indexOf(0));
+        System.out.println(target2.indexOf(1));
+
+
+
+
+
+
+
+        //For you to do:
+        //
+        //Validate if the string ends with "u" prints the boolean value accordingly.
+        //
+        //Validate if the string ends with "world" prints the boolean value accordingly.
+        //
+        //Validate if the string ends with "are" prints the boolean value accordingly.
+        //
+        //Validate if the string ends with "you" prints the boolean value accordingly.
+
+//        String s1="hello how are you";
+//        System.out.println(s1.endsWith("u"));
+//        System.out.println(s1.endsWith("world"));
+//        System.out.println(s1.endsWith("are"));
+//        System.out.println(s1.endsWith("you"));
+
+
+
+
+
+
+
+        //For you to do:
+        //
+        //Create String str1= syntax technologies and convert into SYNTAX TECHNOLOGIES
+        //
+        //Create String str2= SYNTAX TECHNOLOGIES and convert into syntax technologies
+        //
+        //Expected Output:
+        //SYNTAX TECHNOLOGIES
+        //syntax technologies
+
+//        String str1=new String("syntax technologies");
+//        String str2=new String("SYNTAX TECHNOLOGIES");
+//
+//        System.out.println(str1.toUpperCase());
+//        System.out.println(str1.toLowerCase());
+
+
+
+
+
+
+
+        //For you to do:
+        //
+        //Create a String named s1 = "hello" check weather string is empty or not
+        //
+        //Create a String named s2 = "" check weather string is empty or not
+        //
+        //Expected Output:
+        //
+        //false
+        //true
+
+//        String s1=new String("hello");
+//        String s2=new String("");
+//
+//        System.out.println(s1.isEmpty());
+//        System.out.println(s2.isEmpty());
+
+
+
+
+
+
+        //For you to do:
+        //Create a String "name" and assign the value "Timmy" to it.
+        //Find out how many characters are there in the given String.
+
+//        String name =new String("Timmy");
+//        System.out.println(name.length());
+
 
 
     }
