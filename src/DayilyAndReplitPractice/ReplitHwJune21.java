2510ee3852c8ae15d8ff35bcebebf0d6a386b892
MRezaSaedi <mreza.saedi@gmail.com>
2022-06-22 17:48:44 -0700

    1st commit 06-22-2022

--- src/DayilyAndReplitPractice/ReplitHwJune21.java
+++ src/DayilyAndReplitPractice/ReplitHwJune21.java
@@ -2,7 +2,120 @@ package DayilyAndReplitPractice;
 
 public class ReplitHwJune21 {
 
+
+
     /*
+    Create one method as syntaxTechnologies and write the logic for that method in println statement as "Welcome to Syntax Technologies!"
+
+    Create another method as syntaxStudents and write the logic for that method in println statement as "Welcome Syntax Students!"
+
+    Call both methods
+
+    Expected Output:
+
+    Welcome to Syntax Technologies!
+    Welcome Syntax Students!
+     */
+
+//    void syntaxTechnologies (){
+//        System.out.println("Welcome to Syntax Technologies!");
+//    }
+//
+//    void syntaxStudents (){
+//        System.out.println("Welcome Syntax Students!");
+//    }
+
+
+    /*
+    Create a method name as newLine that should have print statement inside the method body as "newLine method implementation"
+
+    Create a method name as displayLine that should have print statement inside the method body as "displayLine method implementation"
+
+    Call both methods
+
+    Expected Output:
+
+    newLine method implementation
+    displayLine method implementation
+     */
+
+//    void newLine(){
+//        System.out.println("newLine method implementation");
+//    }
+//
+//    void displayLine(){
+//        System.out.println("displayLine method implementation");
+//    }
+
+
+
+    /*
+    Create method name it as newLine
+
+    Add print statement inside method body as "newLine method implementation"
+
+    Call newLine method three times
+
+    Expected Output:
+
+    newLine method implementation
+    newLine method implementation
+    newLine method implementation
+     */
+
+//    void newLine(){
+//        System.out.println("newLine method implementation");
+//        System.out.println("newLine method implementation");
+//        System.out.println("newLine method implementation");
+//
+//    }
+
+
+//    void m1(){
+//        System.out.println("m1 method implementation");
+//    }
+//
+//    void m2(){
+//        System.out.println("m2 method implementation");
+//    }
+
+
+
+    public static void main(String[] args) {
+
+
+//        ReplitHwJune21 obj=new ReplitHwJune21();
+//        obj.syntaxTechnologies();
+//        obj.syntaxStudents();
+
+//        ReplitHwJune21 obj=new ReplitHwJune21();
+//        obj.newLine();
+//        obj.displayLine();
+
+
+
+//        ReplitHwJune21 obj=new ReplitHwJune21();
+//        obj.newLine();
+
+        /*
+        create method and name as m1. Inside your method body add print statement as "m1 method implementation"
+
+        create another method and name as m2. Inside your method body add print statement as "m2 method implementation"
+
+        Call these two methods in the main method
+
+        Expected Output:
+
+        m1 method implementation
+        m2 method implementation
+         */
+
+//        ReplitHwJune21 obj= new ReplitHwJune21();
+//        obj.m1();
+//        obj.m2();
+
+
+        /*
         Instantiate and StringBuffer class
 
         Append Value "Hello" to it
@@ -15,11 +128,24 @@ public class ReplitHwJune21 {
 
         HELLO WORLD
      */
-    public static void main(String[] args) {
-        StringBuffer obj=new StringBuffer("HELLO");
-        StringBuffer name=obj.append(" WORLD");
 
-        System.out.println(name);
+//        StringBuffer obj=new StringBuffer("HELLO");
+//        StringBuffer name=obj.append(" WORLD");
+//
+//        System.out.println(name);
+
+
+        /*
+    How would you reverse a String using StringBuffer Class
+    Given String = "Hello Friends"
+
+    Expected Output:
+
+    sdneirF olleH
+     */
+
+//        StringBuffer s=new StringBuffer("Hello Friends");
+//        System.out.println(s.reverse());
     }
 
 
