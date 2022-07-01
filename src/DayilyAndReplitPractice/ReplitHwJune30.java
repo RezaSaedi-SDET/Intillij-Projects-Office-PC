3722437320205280b6d885929ea2ea17ec82eff5
MRezaSaedi <mreza.saedi@gmail.com>
2022-06-30 19:17:51 -0700

    1st commit 06-27-2022

---
+++ src/DayilyAndReplitPractice/ReplitHwJune30.java
@@ -0,0 +1,89 @@
+package DayilyAndReplitPractice;
+
+public class ReplitHwJune30 {
+
+    /*
+    Question 132
+
+    For you to do:
+
+    Complete the reduce10 method by making it subtract 10 from every element of the double array nums.
+
+    The method should static and it should return a new 2D array object
+
+    Print values from new array in the format below input
+
+    1 2 3 4
+    4 5 6 7
+    1 3 5 7
+    Expected Output:
+
+    -9 -8 -7 -6
+    -6 -5 -4 -3
+    -9 -7 -5 -3
+     */
+
+    public static int reduce10(){
+        int[][] nums = {
+                {1,2,3,4},
+                {4,5,6,7},
+                {1,3,5,7}
+        };
+        for (int i=0;i<nums.length;i++){
+            for (i=0;i<nums.length[i];i++){
+
+            }
+
+        }
+
+        return reduce10();
+    }
+
+    public static void main(String[] args) {
+        ReplitHwJune30 obj=new ReplitHwJune30();
+
+        //reduce10(a);
+
+
+    }
+
+    /*
+    Write a method header on line two with the following specs:
+
+    Returns:
+
+    a String
+    Name:
+
+    thirdLetter
+    Parameters:
+
+    a String called s
+    Then complete the method by programming the following behavior
+
+    Returns every 3rd letter of the String s,
+    starting from the first letter.
+    See below examples.
+    Examples:
+
+    thirdLetter("hello there") ==> "hltr"
+    thirdLetter("technology") ==> "thly"
+     */
+//    String thirdLetter(String s){
+//
+//        for (int i=0;i<s.length();i++){
+//            if (i%3==0) {
+//                System.out.print(s.charAt(i));
+//            }
+//            }
+//        return s;
+//        }
+//
+//    public static void main(String[] args) {
+//        ReplitHwJune30 obj=new ReplitHwJune30();
+//        obj.thirdLetter("hello there");
+//        System.out.println();
+//        obj.thirdLetter("technology");
+//    }
+    }
+
